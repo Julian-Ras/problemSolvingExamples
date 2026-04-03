8c1fdbbe5b06dba7e2ce6a0ec53f92dedd2f9056
Julszzz <julian.rascone@gmail.com>
2026-04-02 18:02:58 -0700

    Add commentary to highlightWordArea.py

--- higlightWordArea.py
+++ higlightWordArea.py
@@ -1,23 +1,30 @@
-# def highlightWordArea(h, word):
-#     alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    
-#     height = 0
-#     width = 0
-
-#     for i in word:
-#         for j in alphabet:
-#             if j == i:
-#                 width += 1
-#                 if height < h[alphabet.index(j)]:
-#                     height = h[alphabet.index(j)]
-#                 break
-#     return height * width
+"""
+There is a list of 26 character heights (h) aligned by index to their letters. For example, 'a' is at index 0
+and 'z' is at index 25. There will also be a string. Using the letter heights given, determine the area 
+of the rectangle highlight in mm^2 assuming all letters are 1 mm wide.
+"""
 
 def highlightWordArea(h, word):
-    list_ord = [ord(x) - ord('a') for x in word]
-    print(list_ord)
-    list_value = [h[x] for x in list_ord]
-    return len(list_value) * max(list_value)
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    height = 0
+    width = 0
+
+    for i in word:
+        for j in alphabet:
+            if j == i:
+                width += 1
+                if height < h[alphabet.index(j)]:
+                    height = h[alphabet.index(j)]
+                break
+    return height * width
+
+
+# Using ord function
+# def highlightWordArea(h, word):
+#     list_ord = [ord(x) - ord('a') for x in word]
+#     list_value = [h[x] for x in list_ord]
+#     return len(list_value) * max(list_value)
 
 
 # h = [1,3,1,3,1,4,1,3,2,5,5,5,5,1,1,5,5,1,5,2,5,5,5,5,5,5]
